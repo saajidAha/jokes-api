b7ed7172cb16a9474b5a63cfc3d396a9bf25f800
Saajid Ahamed <saajidahamed1@gmail.com>
2024-10-17 18:41:01 +0530

    initial commit with .gitignore

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import ejs from 'ejs';
+import axios from 'axios';
+
+const API_URL = ' https://v2.jokeapi.dev/joke';
+const port = 3000;
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res)=>{
+    res.render('index.ejs',{content: 'waiting for data.............'})
+})
+
+app.post('/generate', async(req, res)=>{
+    const category = req.body.type;
+    console.log(category)
+    try{
+        const response = await axios.get(`${API_URL}/${category}`);
+        console.log(response);
+        const data =  response.data.setup;
+        console.log(data);
+        res.render('index.ejs',{content: data});
+    }
+    catch(error){
+        console.log(error);
+        res.sendStatus(500).render('index.ejs', {content: 'small error with the server. please try again later.'})
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`listening at port ${port}`)
+})
